671e07ca8c522953d5114136d3b238b279c551e1
sustainablelab <sustainablelab@gmail.com>
2025-09-06 20:03:35 -0400

    Turn MemBuff into a struct, return NULL when array runs out of room, and improve formatting of unit tests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # About
 
-Come up with a simple memory stack for easily allocating memory needed for the
+Come up with a simple memory buffer for easily allocating memory needed for the
 current level of a game.
 
 This is based on the simple idea presented by Casey Muratori in [Episode 43 of
@@ -9,20 +9,20 @@ Handmade Hero][^1_1]. Jump to the 43:00 mark for the initial code and the
 
 [^1_1]: https://youtube.com/watch?v=IJYTwhqfKLg&si=aGv8rugixUBomGtA
 
-# MemStack
+# MemBuff
 
-The memory stack, `MemStack`, is like a function stack frame, but it is for a
-game level rather than for a function. Unlike a function which automatically
-pops all of its local variables off the stack, I will never pop anything, I
-will simply "free" the entire array by resetting the pointer to the head of the
-array.
+The memory buffer, `MemBuff`, is like a function stack frame, but it is for a
+game level rather than for a function. A function automatically pops all of its
+local variables off the stack, but I will never pop anything, I will simply
+"free" the entire array by resetting the pointer to the head of the array.
 
-i.e., `MemStack` is just:
+To summarize, `MemBuff` is just:
 
-- a giant array of memory with a lifetime that is the program lifetime
-- a pointer to the first address in the giant array (for reset/free)
-- a pointer to the next available address in the giant array (for allocating)
-- functions for pushing arrays and structs onto this giant array
+- `array`: a giant array of memory with a lifetime that is the program lifetime
+- `start`: a pointer to the first address in the giant array (for initialization and for memory free)
+- `head`: a pointer to the next available address in the giant array (for the caller to allocate)
+- functions `_init`, `_push_array`, and `_push_strcut` for pushing arrays and
+  structs onto this giant array
 
 # TDD
 
@@ -31,7 +31,50 @@ I'll use TDD for this. I put the beginnings of a simple test framework in
 
 # Status
 
-Initial TDD framework is in there with two passing tests.
+## TDD status
+
+* TDD has 7 passing tests.
+* All tests follow the format:
+    * Setup
+    * Exercise
+    * Test
+* TDD has macros:
+    * `TEST_INTeq(a,b,...)` to test if `a == b` for integers
+    * `TEST_PTReq(a,b,...)` to test if `a == b` for pointers
+    * `TEST_PTRgt(a,b,...)` to test if `a > b`  for pointers
+    * The final `...` argument is a `puts("Name or description of test")`
+
+The test output looks like this:
+
+```
+Running tests...
+ 1. PASS: MemBuff_init(&memBuff) sets memBuff.start to point at the first address in memBuff.array
+ 2. PASS: MemBuff_push_array(&memBuff, 2, uint8_t) advances the MemBuff.head pointer by 2 bytes
+ 3. PASS: MemBuff_push_array(...) returns NULL if array cannot fit in memBuff
+ 4. PASS: Given memBuff.array is empty, MemBuff_push_array(&memBuff, 2, uint8_t) returns a pointer to the first address in memBuff.array
+ 5. PASS: Given memBuff.array is not empty, MemBuff_push_array(&memBuff, 2, uint8_t) returns a pointer to an address after the first address in memBuff.array
+ 6. PASS: Caller can read and write the first byte after MemBuff_push_array(&memBuff, 2, uint8_t)
+ 7. PASS: Caller can read and write the second byte after MemBuff_push_array(&memBuff, 2, uint8_t)
+        Inspect memBuff:
+                memBuff starts here:          0x7ffe449febe0
+                memBuff head points here:     0x7ffe449febe2
+                memBuff is 8 bytes long
+                Last byte in memBuff is here: 0x7ffe449febe7
+                Contents of memBuff:
+                1,2,0,0,0,0,0,0,
+ 8. FAIL: A failing test looks like this
+        Run TEST in run_tests_for_MemBuff(), src/main.c|313:
+        Expect '1 == 2' but '1 == 1'
+
+------------
+Test summary
+------------
+1 (fail) and  7 (pass) out of 8 tests (total)
+```
+
+The word `PASS` is green foreground text and `FAIL` has a red background.
+
+## Macro to pass array data type
 
 I used a macro to pass data types instead of having to call `sizeof()` in the
 calling code:
@@ -39,22 +82,22 @@ calling code:
 The macro lets me do this:
 
 ```c
-MemStack_push_array(count, type)
+MemBuff_push_array(count, type)
 ```
 
 Instead of this:
 
 ```c
-MemStack_push_array(count, sizeof(*ptr))
+MemBuff_push_array(count, sizeof(*ptr))
 ```
 
 In the former, the macro and function definitions are:
 
 ```c
-#define MemStack_push_array(count, type) (type *)MemStack_push_array_((count)*sizeof(type))
-uint8_t* MemStack_push_array_(size_t size) {
-    uint8_t* address = MemStack_ptr;
-    MemStack_ptr += size;
+#define MemBuff_push_array(count, type) (type *)MemBuff_push_array_((count)*sizeof(type))
+uint8_t* MemBuff_push_array_(size_t size) {
+    uint8_t* address = MemBuff_ptr;
+    MemBuff_ptr += size;
     return address;
 }
 ```
@@ -62,15 +105,132 @@ uint8_t* MemStack_push_array_(size_t size) {
 In the latter (without the macro), the function definition is:
 
 ```c
-uint8_t* MemStack_push_array(int count, int size) {
-    uint8_t* address = MemStack_ptr;
-    MemStack_ptr += count*size;
+uint8_t* MemBuff_push_array(int count, int size) {
+    uint8_t* address = MemBuff_ptr;
+    MemBuff_ptr += count*size;
     return address;
 }
 ```
 
+## Turn MemBuff into a struct
+
+Instead of using a single global memory buffer, I created a `MemBuff` type:
+
+```c
+typedef struct {
+    uint8_t array[MEMBUFF_LEN];
+    uint8_t* start;
+    uint8_t* head;
+} MemBuff;
+```
+
+Define a `MemBuff` to obtain the memory from the OS:
+
+```c
+MemBuff memBuff;
+```
+
+Above I define `memBuff`.
+
+### MemBuff array does not need to be initialized
+
+Defining a `MemBuff` in this way does allocate the memory for member `array`.
+And this allocation happens in whatever scope the declaration happens. As long
+as I do `MemBuff membuff;` in `main()`, the lifetime of member `array` is the
+lifetime of the program because `main()` does not return until the end of the
+program.
+
+I don't initialize the struct because there is nothing to initialize.
+
+For example, I can use array initialization to initialize the first few values
+of member `array`, e.g.:
+
+```c
+MemBuff memBuff = {.array={1,2,3,4}}
+```
+
+This is useful for unit tests. But in the actual application, I never care what
+values are in the array for any address from `head` up to the end of the array.
+
+## Initialize MemBuff start and head pointers
+
+And there is no way to assign the pointers `start` and `head` in the structure
+initialization. But I do need to assign these pointers, so I created an `_init`
+function:
+
+```c
+MemBuff_init(&memBuff);
+```
+
+All it does is assign `start` and `head` to both point at the first address in
+`array`.
+
+```c
+void MemBuff_init(MemBuff* const memBuff) {
+    memBuff->start = memBuff->array;
+    memBuff->head = memBuff->array;
+}
+```
+
+I define and initialize on the same line:
+
+```c
+MemBuff memBuff; MemBuff_init(&memBuff);
+```
+
+## Push array of bytes onto MemBuff
+
+*Remember I'm "getting" an array of bytes from the MemBuff pool. "Push" is
+referring to the idea of the buffer as a stack. "Push" just means I'm reserving
+bytes for the caller to use.*
+
+To push arrays:
+
+```c
+#define MemBuff_push_array(memBuff, count, type) (type *)MemBuff_push_array_(memBuff, (count)*sizeof(type))
+uint8_t* MemBuff_push_array_(MemBuff* const memBuff, size_t size) {
+    // Return NULL if there is not enough room left in the buffer to fit the array
+    if (memBuff->head + size >= memBuff->start + MEMBUFF_LEN) return NULL;
+
+    // Otherwise, advance head and return the base address of the array
+    uint8_t* base_address = memBuff->head;
+    memBuff->head += size;
+    return base_address;
+}
+```
+
+Returning `NULL` indicates there is not enough room to hold the array the caller is asking for.
+
+## Inspect the MemBuff
+
+Here is a helper function (handy during testing) to inspect the memory buffer:
+
+```c
+void check_membuff(MemBuff* const memBuff) {
+    puts("Inspect memBuff:");
+    printf("\tmemBuff starts here:          %p\n", (void *)memBuff->start);
+    printf("\tmemBuff head points here:     %p\n", (void *)memBuff->head);
+    printf("\tmemBuff is %d bytes long\n", MEMBUFF_LEN);
+    printf("\tLast byte in memBuff is here: %p\n", (void *)(memBuff->start + MEMBUFF_LEN - 1));
+    printf("\tContents of memBuff:\n\t\t");
+    for (int i=0; i<MEMBUFF_LEN; i++) printf("%d,",memBuff->array[i]);
+    puts("");
+}
+```
+
+Check like this: `check_membuff(&memBuff)`. Example output:
+
+```
+Inspect memBuff:
+        memBuff starts here:          0x7ffffea98710
+        memBuff head points here:     0x7ffffea98712
+        memBuff is 8 bytes long
+        Last byte in memBuff is here: 0x7ffffea98717
+        Contents of memBuff:
+                1,2,0,0,0,0,0,0,
+```
+
 # Next steps
 
-1. Throw an assertion error (program terminates) when I run out of memory (this is a temporary placeholder -- long term I probably want to return NULL or -1 to indicate I ran out of memory).
 1. Make this work with arrays of larger data types (like floats).
 1. Make a similar function for pushing structs.
--- src/main.c
+++ src/main.c
@@ -1,42 +1,77 @@
-/** MG_mem_stack
+/** MG_membuff
  *
- * The idea is to create the most basic kind of memory arena: a stack.
+ * The idea is to create the most basic kind of memory arena: a buffer with a pointer
+ * to the next empty location.
  *
- * I want the same kind of stack I get with a function's stack frame, except I want
- * this stack to be independent of the lifetime of any function's stack frame.
+ * I want:
  *
- * This first came up when, while working on broken-ladder, I wanted to control the
- * update physics timing: instead of updating point locations on every frame (every
- * iteration of the game loop), I only want to update them at some slower rate. I was
- * using the stack frame of my render() function to allocate the memory for the point
- * locations. So this was allocated (and automatically deallocated) on every frame. I
- * wanted the point locations to return to their initial value, then as part of the
- * animation, I would push the point locations slightly, based on a random value or
- * some other animation effect.
+ * - the same effortless allocation I get with a function's stack frame
+ * - control over the lifetime of the buffer
  *
- * For the value to persist across frames, I cannot use the render() stackframe
- * because all of that memory is deallocated when the function exits.
+ * gameMemBuff = MG_MemBuff()
+ */
+/** Motiviation
+ *
+ * This first came up while working on broken-ladder.
+ *
+ * To draw the player, I started out with the player's vertices defined in the
+ * render() function, i.e., these vertices had the lifetime of the render() function.
+ * This was fine for animation at first. The initial values of the points were
+ * hard-coded in render(). The animation would push the point locations slightly,
+ * based on a random value or some other animation effect. Since the initial values
+ * were hard-coded, the next frame would "reset" the points to their initial values.
+ * This gave the player a fluid appearance without the player's position randomly
+ * drifting around the screen.
+ *
+ * But then I wanted to decouple the animation update rate from the video frame rate.
+ * This means the vertices had to persist after exiting render() so that I could draw
+ * the vertices for multiple frames with the same value.
+ *
+ * Stack frame variables do not persist across function calls. I thought of declaring
+ * them 'static', but I'm not sure if that is thread-safe.
  *
  * So I simply made the point locations global.
  *
- * This is fine, but now when I want to add more entities with their own array of
- * points, I'm going to have a lot to keep track of. I can simply create a ton of
- * global arrays, but this is hard to organize in source code and it means I need to
- * define all of my entitities ahead of time: I cannot dynamically create more
- * entities.
+ * This is fine for a single entity. But my game will have many entities. Each will
+ * have their own array of points, I'm going to have a lot to keep track of and a lot
+ * of tedious setup code to write. A ton of global arrays is hard to organize in code.
+ * I have to define ALL of my entitities ahead of time in code: I cannot dynamically
+ * create more entities. And how would I store the entities in a saved game file and
+ * then read them back later?
+ */
+/** push_array(), push_struct(), and level lifetime
  *
- * So looking ahead, I want to create my own giant stack that I can simply push arrays
- * or structs onto. The MemStack_push_array() or MemStack_push_struct() should return
- * a pointer to the entity I pushed. I don't need a way to free this memory. The
- * lifetime of all these variables will be similar: perhaps the lifetime is each level
- * of the game (so changing levels ends the life of all variables on this stack and I
- * get a fresh empty stack). I guess then this is like a stackframe for levels!
+ * So looking ahead, I want to create my own giant buffer that I can simply push
+ * arrays or structs onto. The MemBuff_push_array() or MemBuff_push_struct() should
+ * return a pointer to the entity I pushed. I don't need a way to free this memory.
+ * The lifetime of all these variables will be similar: perhaps the lifetime is each
+ * level of the game (so changing levels ends the life of all variables in this buffer
+ * and I get a fresh empty buffer). I guess then this is like a stack frame for
+ * levels!
  *
- * So the idea is to reserve a giant array of memory with a lifetime that lasts for
- * the entire program. Then the MemStack is just a pointer to the next available
+ * So the idea is to reserve a giant array of memory with a lifetime that is the
+ * program's lifetime. Then the MemBuff is just a pointer to the next available
  * location in this array and a collection of functions for pushing different types
- * onto the MemStack, i.e., for allocating memory from this array and updating the
- * MemStack pointer.
+ * onto the MemBuff, i.e., for allocating memory from this array and updating the
+ * MemBuff pointer.
+ *
+ * A single buffer solves some problems, but not all of them.
+ */
+/** Two buffers: Index Buffer and Data Buffer
+ * To make the contents dynamic, I think I need two buffers: a data buffer and an
+ * index buffer for interpreting the contents of the data buffer.
+ *
+ * For example, the player has a points_index struct that is a 'count' and the base
+ * address of the points. The player as a lot of other data, but right now I am only
+ * thinking of the data that render() needs. The render() will use the points_index
+ * struct to get the base address and the 'count' to know how many points to iterate
+ * over.
+ *
+ * When loading a saved game from file, the base address of the data buffer will be
+ * different (the base address changes each time the program runs). To make the
+ * addresses in the index buffer robust against this, the index buffer stores offsets
+ * from the data buffer base address rather than absolute addresses in the data
+ * buffer.
  */
 
 #include <stdio.h>
@@ -49,84 +84,229 @@
 int MGUT_total = 0;
 int MGUT_pass = 0;
 int MGUT_fail = 0;
-bool MGUT_show_tests = true;
+bool MGUT_show_failing_tests = true;
+bool MGUT_show_passing_tests = true;
 
-#define HEADING(TYPE) do\
+////////////////////////////////////////
+// PRIVATE UNIT TEST MACROS FOR PRINTING
+////////////////////////////////////////
+#define _LOG_TESTNAME(CODE_TO_PRINT_RESULT, CODE_TO_PRINT_TESTNAME) do\
     {\
-    printf("\nRun %s in %s(), %s|%d:", #TYPE, __func__, __FILE__, __LINE__);\
+        printf("%2d. ", MGUT_total + 1);\
+        CODE_TO_PRINT_RESULT\
+        CODE_TO_PRINT_TESTNAME\
     } while(0)
 
-// Test ints a,b are equal
-#define TEST_INTeq(a,b) if (a!=b)\
+#define _PRINT_RUN_MSG(TYPE) do\
     {\
-        if (MGUT_show_tests)\
+    printf("\tRun %s in %s(), %s|%d:\n", #TYPE, __func__, __FILE__, __LINE__);\
+    } while(0)
+
+#define _PASSING_TEST(CODE_TO_PRINT_TESTNAME) do\
+    {\
+        if (MGUT_show_passing_tests)\
         {\
-            HEADING(TEST);\
-            printf(" \033[41mFAIL\033[00m\n\t");\
-            printf("Expect '%s == %d' but '%s == %d'",\
+            _LOG_TESTNAME(printf("\033[32mPASS\033[00m: ");, CODE_TO_PRINT_TESTNAME);\
+            MGUT_pass++;\
+        }\
+    } while(0);
+
+////////////////
+// UNIT TEST API
+////////////////
+// Test ints a,b are equal
+#define TEST_INTeq(a,b,CODE_TO_PRINT_TESTNAME) if (a!=b)\
+    {\
+        if (MGUT_show_failing_tests)\
+        {\
+            _LOG_TESTNAME(printf("\033[41mFAIL\033[00m: ");, CODE_TO_PRINT_TESTNAME);\
+            _PRINT_RUN_MSG(TEST);\
+            printf("\tExpect '%s == %d' but '%s == %d'\n\n",\
                             #a,    b,      #a,    a);\
         }\
         MGUT_fail++;\
     }\
-    else MGUT_pass++;\
+    else _PASSING_TEST(CODE_TO_PRINT_TESTNAME)\
     MGUT_total++;
 
 // Test pointers a,b are equal
-#define TEST_PTReq(a,b) if (a!=b)\
+#define TEST_PTReq(a,b,CODE_TO_PRINT_TESTNAME) if (a!=b)\
     {\
-        if (MGUT_show_tests)\
+        if (MGUT_show_failing_tests)\
         {\
-            HEADING(TEST);\
-            printf(" \033[41mFAIL\033[00m\n\t");\
-            printf("Expect '%s == %p' but '%s == %p'",\
+            _LOG_TESTNAME(printf("\033[41mFAIL\033[00m: ");, CODE_TO_PRINT_TESTNAME);\
+            _PRINT_RUN_MSG(TEST);\
+            printf("\tExpect '%s == %p' but '%s == %p'\n\n",\
                             #a,(void *)b,  #a,(void *)a);\
         }\
         MGUT_fail++;\
     }\
-    else MGUT_pass++;\
+    else _PASSING_TEST(CODE_TO_PRINT_TESTNAME)\
+    MGUT_total++;
+
+// Test pointer a > pointer b
+#define TEST_PTRgt(a,b,CODE_TO_PRINT_TESTNAME) if (a<=b)\
+    {\
+        if (MGUT_show_failing_tests)\
+        {\
+            _LOG_TESTNAME(printf("\033[41mFAIL\033[00m: ");, CODE_TO_PRINT_TESTNAME);\
+            _PRINT_RUN_MSG(TEST);\
+            printf("\tExpect '%s > %p' but '%s == %p'\n\n",\
+                            #a,(void *)b,  #a,(void *)a);\
+        }\
+        MGUT_fail++;\
+    }\
+    else _PASSING_TEST(CODE_TO_PRINT_TESTNAME)\
     MGUT_total++;
 
 ///////////////////////////
-// MemStack CODE UNDER TEST
+// MemBuff CODE UNDER TEST
 ///////////////////////////
-/** MemStack is an array of MEMSTACK_LEN bytes
- * TODO: put the variables below inside of a struct named MemStack (this is the doc
- * string for that struct!)
- * \param
+/** MemBuff is a buffer of MEMBUFF_LEN bytes
+ * \param array this is the pool of memory for allocating contiguous blocks to the caller
+ * \param start this always points at the base address of 'array'
+ * \param head this points to the next available memory address in 'array'
  */
-#define MEMSTACK_LEN 256
-uint8_t MemStack_arr[MEMSTACK_LEN];
-uint8_t* MemStack_ptr = MemStack_arr;
+#define MEMBUFF_LEN 8
+typedef struct {
+    uint8_t array[MEMBUFF_LEN];
+    uint8_t* start;
+    uint8_t* head;
+} MemBuff;
+
+void MemBuff_init(MemBuff* const memBuff) {
+    memBuff->start = memBuff->array;
+    memBuff->head = memBuff->array;
+}
+
 /**
- * Return a pointer to 'count' * 'size' number of bytes
+ * Reserve in the `memBuff` (memory buffer) an array of 'count' 'type'.
+ * \param memBuff MemBuff (memory buffer) 
  * \param count Array length
- * \param size Size of one element in the array
+ * \param type Data type, e.g., uint8_t or SDL_FPoint.
+ *              The macro converts 'type' into a size_t with sizeof(type)
+ * \return Return a pointer to the base address of the array. Return NULL if memBuff
+ * does not have enough room to store the array.
  */
-#define MemStack_push_array(count, type) (type *)MemStack_push_array_((count)*sizeof(type))
-uint8_t* MemStack_push_array_(size_t size) {
-    uint8_t* address = MemStack_ptr;
-    MemStack_ptr += size;
-    return address;
+#define MemBuff_push_array(memBuff, count, type) (type *)MemBuff_push_array_(memBuff, (count)*sizeof(type))
+uint8_t* MemBuff_push_array_(MemBuff* const memBuff, size_t size) {
+    // Return NULL if there is not enough room left in the buffer to fit the array
+    if (memBuff->head + size >= memBuff->start + MEMBUFF_LEN) return NULL;
+
+    // Otherwise, advance head and return the base address of the array
+    uint8_t* base_address = memBuff->head;
+    memBuff->head += size;
+    return base_address;
+}
+
+/**
+ * Test helper to check a memBuff by printing its pointer addresses and its contents.
+ * \param memBuff address of the MemBuff that I want to inspect
+ */
+void check_membuff(MemBuff* const memBuff) {
+    puts("\tInspect memBuff:");
+    printf("\t\tmemBuff starts here:          %p\n", (void *)memBuff->start);
+    printf("\t\tmemBuff head points here:     %p\n", (void *)memBuff->head);
+    printf("\t\tmemBuff is %d bytes long\n", MEMBUFF_LEN);
+    printf("\t\tLast byte in memBuff is here: %p\n", (void *)(memBuff->start + MEMBUFF_LEN - 1));
+    printf("\t\tContents of memBuff:\n\t\t");
+    for (int i=0; i<MEMBUFF_LEN; i++) printf("%d,",memBuff->array[i]);
+    puts("");
 }
 
 /////////////
 // UNIT TESTS
 /////////////
-void run_tests_for_MG_stack() {
-    { // MemStack_push_array() returns a pointer
-        puts("Given MemStack_arr is empty, "
-                "MemStack_push_array(2, sizeof (*byte_array) "
-                "returns a pointer to the first address in MemStack_arr");
-        uint8_t* expected = &MemStack_arr[0];
-        uint8_t* byte_array = MemStack_push_array(2, uint8_t);
-        TEST_PTReq(byte_array, expected);
+void run_tests_for_MemBuff() {
+    { // MemBuff_init() sets memBuff.start to first address in memBuff.array
+        // Setup and Exercise
+        MemBuff memBuff = {.array={1,2,3,4}}; MemBuff_init(&memBuff);
+
+        // Test
+        uint8_t *expected = &memBuff.array[0];
+        TEST_PTReq(memBuff.start, expected,
+                puts("MemBuff_init(&memBuff) sets memBuff.start to "
+                     "point at the first address in memBuff.array");
+                );
     }
-    { // MemStack_push_array() advances the MemStack pointer
-        puts("MemStack_push_array(2, sizeof (*byte_array) "
-                "advances the MemStack pointer by 2 bytes");
-        uint8_t* expected = MemStack_ptr+2;
-        MemStack_push_array(2, uint8_t);
-        TEST_PTReq(MemStack_ptr,expected);
+    { // MemBuff_push_array() advances the MemBuff.head pointer
+        // Setup
+        MemBuff memBuff; MemBuff_init(&memBuff);
+
+        // Exercise
+        MemBuff_push_array(&memBuff, 2, uint8_t);
+
+        // Test
+        uint8_t* expected = &memBuff.array[2];
+        TEST_PTReq(memBuff.head,expected,
+                puts("MemBuff_push_array(&memBuff, 2, uint8_t) "
+                     "advances the MemBuff.head pointer by 2 bytes");
+                );
+    }
+    { // MemBuff_push_array() returns NULL if array cannot fit in memBuff
+        // Setup
+        MemBuff memBuff; MemBuff_init(&memBuff);
+
+        // Exercise
+        uint8_t* byte_array = MemBuff_push_array(&memBuff, MEMBUFF_LEN+1, uint8_t);
+
+        // Test
+        TEST_PTReq(byte_array,NULL,
+                puts("MemBuff_push_array(...) returns NULL if "
+                     "array cannot fit in memBuff");
+                );
+    }
+    { // If memBuff is empty, MemBuff_push_array() returns a pointer to first address
+        // Setup
+        MemBuff memBuff; MemBuff_init(&memBuff);
+
+        // Exercise
+        uint8_t* byte_array = MemBuff_push_array(&memBuff, 2, uint8_t);
+
+        // Test
+        uint8_t* expected = &memBuff.array[0];
+        TEST_PTReq(byte_array, expected,
+                puts("Given memBuff.array is empty, "
+                     "MemBuff_push_array(&memBuff, 2, uint8_t) "
+                     "returns a pointer to the first address in memBuff.array");
+                );
+    }
+    { // If memBuff is not empty, MemBuff_push_array() returns a pointer to a later address
+        // Setup
+        MemBuff memBuff; MemBuff_init(&memBuff);
+        MemBuff_push_array(&memBuff, 2, uint8_t);
+
+        // Exercise
+        uint8_t* byte_array = MemBuff_push_array(&memBuff, 2, uint8_t);
+
+        // Test
+        uint8_t* expected = &memBuff.array[0];
+        TEST_PTRgt(byte_array, expected,
+                puts("Given memBuff.array is not empty, "
+                     "MemBuff_push_array(&memBuff, 2, uint8_t) "
+                     "returns a pointer to an address after the "
+                     "first address in memBuff.array");
+                );
+    }
+    { // Caller can r/w first N bytes after pushing an N-byte array
+        // Setup
+        MemBuff memBuff = {.array={0,0}}; MemBuff_init(&memBuff);
+
+        // Exercise
+        uint8_t* byte_array = MemBuff_push_array(&memBuff, 2, uint8_t);
+
+        // Test
+        uint8_t a = 1, b = 2;
+        byte_array[0] = a; byte_array[1] = b;
+        TEST_INTeq(byte_array[0], a,
+                puts("Caller can read and write the first byte after "
+                     "MemBuff_push_array(&memBuff, 2, uint8_t)");
+                );
+        TEST_INTeq(byte_array[1], b,
+                puts("Caller can read and write the second byte after "
+                     "MemBuff_push_array(&memBuff, 2, uint8_t)");
+                );
+        check_membuff(&memBuff);
     }
 }
 
@@ -135,7 +315,7 @@ void run_tests_for_MG_stack() {
 //////////////
 int main(void) {
     puts("Running tests...");
-    run_tests_for_MG_stack();
+    run_tests_for_MemBuff();
     puts("------------");
     puts("Test summary");
     puts("------------");
